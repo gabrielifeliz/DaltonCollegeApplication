4342e16734c205a454bd637fca7873d4f37ec098
GBTC441017UR <fadhld92@gmail.com>
2018-07-10 13:56:54 -0400

    just messing with test controller

--- src/main/java/com/example/demo/repositories/AppUserRepository.java
+++ src/main/java/com/example/demo/repositories/AppUserRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.models.authentication.AppUser;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+    AppUser findByUsername(String s);
 }
