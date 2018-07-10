36034a709ae190a4963165a853f2e4b96db8032b
GBTC441014ur <deffpatou@gmail.com>
2018-07-10 13:42:42 -0400

    not much changed

--- src/main/java/com/example/demo/repositories/UserRoleRepository.java
+++ src/main/java/com/example/demo/repositories/UserRoleRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.models.authentication.repositories;
 
+import com.example.demo.models.authentication.UserRole;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
+    UserRole findByRole(String role);
 }
