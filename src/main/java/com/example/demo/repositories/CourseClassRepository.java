ad55af998fbea4e177258c7abdbf51e0af63a0b0
Gabriel I Feliz <gfeliz3@montgomerycollege.edu>
2018-07-11 11:13:57 -0400

    fixed first error

---
+++ src/main/java/com/example/demo/repositories/CourseClassRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Course;
+import com.example.demo.models.CourseClass;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseClassRepository extends CrudRepository<CourseClass, Long> {
+    Iterable<CourseClass> findAllByCourse(Course course);
+}
